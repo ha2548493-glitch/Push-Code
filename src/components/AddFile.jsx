7f0d90067acc71ecc9db2ffff6adea9e06347c01
ha2548493-glitch <ha2548493@gmail.com>
2026-02-20 14:33:24 +0500

    update code

--- src/components/AddFile.jsx
+++ src/components/AddFile.jsx
@@ -3,45 +3,11 @@ import React from 'react'
 const AddFile = () => {
   return (
     <div>This is the added file
-      <h1>
-        Husnain Ali
-      </h1>
-        <h1>
-        Husnain Ali
-      </h1>
-        <h1>
-        Husnain Ali
-      </h1>
-        <h1>
-        Husnain Ali
-      </h1>
-        <h1>
-        Husnain Ali
-      </h1>
-        <h1>
-        Husnain Ali
-      </h1>
-      <h1>I again updated  this file .</h1>
-      <h1>check out the changes</h1>
-      <h1>tell me the changes . i should have to do ?</h1>
-      <h2>
-        This is husnain ali
-      </h2>
-       <h2>
-        This is husnain ali
-      </h2>
-       <h2>
-        This is husnain ali
-      </h2>
-       <h2>
-        This is husnain ali
-      </h2>
-       <h2>
-        This is husnain ali
-      </h2>
-       <h2>
-        This is husnain ali
-      </h2>
+  <h1>I updated  this file .</h1>
+    <h1>I updated  this file .</h1>
+      <h1>I updated  this file .</h1>
+        <h1>I updated  this file .</h1>
+          <h1>I updated  this file .</h1>
     </div>
   )
 }
