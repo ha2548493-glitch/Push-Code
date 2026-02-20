c33c86aec502280531cdd33c59a7620fa7c3c465
Hunain <hunainmazhariqbal@gmail.com>
2026-02-20 14:45:38 +0500

    my updated code

--- src/components/AddFile.jsx
+++ src/components/AddFile.jsx
@@ -3,11 +3,55 @@ import React from 'react'
 const AddFile = () => {
   return (
     <div>This is the added file
+<<<<<<< Updated upstream
   <h1>I updated  this file .</h1>
     <h1>I updated  this file .</h1>
       <h1>I updated  this file .</h1>
         <h1>I updated  this file .</h1>
           <h1>I updated  this file .</h1>
+=======
+      <h1>
+        Husnain Ali
+      </h1>
+        <h1>
+        Husnain Ali
+      </h1>
+        <h1>
+        Husnain Ali
+      </h1>
+        <h1>
+        Husnain Ali
+      </h1>
+        <h1>
+        Husnain Ali
+      </h1>
+        <h1>
+        Husnain Ali
+      </h1>
+      <h1>I again updated  this file .</h1>
+      <h1>check out the changes</h1>
+      <h1>tell me the changes . i should have to do ?</h1>
+      <h2>
+        This is husnain ali
+      </h2>
+       <h2>
+        This is husnain ali
+      </h2>
+       <h2>
+        This is husnain ali
+      </h2>
+       <h2>
+        This is husnain ali
+      </h2>
+       <h2>
+        This is husnain ali
+      </h2>
+       <h2>
+        This is husnain ali
+      </h2>
+      <h1>My name is Hunain Mazhar </h1>
+      <h1>And this is my third update in this file</h1>
+>>>>>>> Stashed changes
     </div>
   )
 }
