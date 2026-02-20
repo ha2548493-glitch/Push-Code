8175b54f5d3d101a241b88597906afb54f0d6684
ha2548493-glitch <ha2548493@gmail.com>
2026-02-20 10:26:44 +0500

    Edit code

--- src/components/AddFile.jsx
+++ src/components/AddFile.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const AddFile = () => {
   return (
-    <div>This is the added file</div>
+    <div>This is the added file
+      <h1>
+        Husnain Ali
+      </h1>
+        <h1>
+        Husnain Ali
+      </h1>
+        <h1>
+        Husnain Ali
+      </h1>
+        <h1>
+        Husnain Ali
+      </h1>
+        <h1>
+        Husnain Ali
+      </h1>
+        <h1>
+        Husnain Ali
+      </h1>
+    </div>
   )
 }
 
